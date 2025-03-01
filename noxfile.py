ba7fc3c9dff32270b67f28fda2ffad7739bfa7fe
Jan Philipp Berg <git.7ksst@aleeas.com>
2025-03-01 17:06:39 +0100

    Add noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,30 @@
+from typing import Final
+
+import nox
+
+nox.options.default_venv_backend = "uv"
+
+PYTHON_VERSION_LIST: Final[list[str]] = ["3.12", "3.13"]
+
+
+@nox.session
+def tidy(session: nox.Session) -> None:
+    """Cleans the code"""
+    session.install("isort")
+    session.run("isort", ".")
+
+    session.install("black")
+    session.run("black", ".")
+
+    session.install("autoflake")
+    session.run("autoflake", "." "-r")
+    session.notify("check")
+
+
+@nox.session(python=PYTHON_VERSION_LIST)
+def check(session: nox.Session) -> None:
+    """Runs static analysis and tests"""
+    session.install("mypy")
+    session.run("mypy")
+
+    session.run("python3", "-m", "unittest", "test/aschenputtel_test.py")
